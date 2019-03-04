9c2ec8c3e5586aed09985c16f4fca44d18458c19
Zeroto521 <Zeroto521@gmail.com>
2019-03-04 20:25:31 +0800

    :construction:

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,5 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-if __name__ == "__main__":
-    app = create_app('production')
-    app.run(host='0.0.0.0', port='80')
+
+app = create_app('production')
