a431509b680c1171952eb8d78d1078c2fd6670eb
Zeroto521 <Zeroto521@gmail.com>
2019-03-12 23:49:48 +0800

    :lipstick: :heavy_minus_sign:
    drop no useful js code

--- MMCs/static/js/script.js
+++ MMCs/static/js/script.js
@@ -1,52 +1,4 @@
 $(function () {
-    var default_error_message = 'Server error, please try again later.';
-
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader('X-CSRFToken', csrf_token);
-            }
-        }
-    });
-
-    $(document).ajaxError(function (event, request, settings) {
-        var message = null;
-        if (request.responseJSON && request.responseJSON.hasOwnProperty('message')) {
-            message = request.responseJSON.message;
-        } else if (request.responseText) {
-            var IS_JSON = true;
-            try {
-                var data = JSON.parse(request.responseText);
-            } catch (err) {
-                IS_JSON = false;
-            }
-            if (IS_JSON && data !== undefined && data.hasOwnProperty('message')) {
-                message = JSON.parse(request.responseText).message;
-            } else {
-                message = default_error_message;
-            }
-        } else {
-            message = default_error_message;
-        }
-        toast(message, 'error');
-    });
-
-    var flash = null;
-
-    function toast(body, category) {
-        clearTimeout(flash);
-        var $toast = $('#toast');
-        if (category === 'error') {
-            $toast.css('background-color', 'red')
-        } else {
-            $toast.css('background-color', '#333')
-        }
-        $toast.text(body).fadeIn();
-        flash = setTimeout(function () {
-            $toast.fadeOut();
-        }, 3000);
-    }
-
     // delete confirm modal
     $('#confirm-delete').on('show.bs.modal', function (e) {
         $('.delete-form').attr('action', $(e.relatedTarget).data('href'));
@@ -67,13 +19,4 @@ $(function () {
     $('#confirm-continue').on('show.bs.modal', function (e) {
         $('.continue-form').attr('action', $(e.relatedTarget).data('href'));
     });
-    // download confirm modal
-    $('#confirm-download').on('show.bs.modal', function (e) {
-        $('.download-form').attr('action', $(e.relatedTarget).data('href'));
-    });
-
-    if (is_authenticated) {
-        setInterval(update_notifications_count, 30000);
-    }
-
 });
